98633283537632a13bcc58cd23bb99b23ac8c633
Shivam Choudhary <45448915+shivamkchoudhary@users.noreply.github.com>
2021-01-13 12:33:45 +0530

    Create main.go

---
+++ Gopher It/main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+/*func main () {
+  fmt.Println("  __      _")
+  fmt.Println("o'')}____//")
+  fmt.Println(" `_/      )")
+  fmt.Println(" (_(_/-(_/ ")
+}*/
+
+func main () {
+  fmt.Println("    `.-::::::-.`    ")
+  fmt.Println(".:-::::::::::::::-:.")
+  fmt.Println("`_:::    ::    :::_`")
+  fmt.Println(" .:( ^   :: ^   ):. ")
+  fmt.Println(" `:::   (..)   :::. ")
+  fmt.Println(" `:::::::UU:::::::` ")
+  fmt.Println(" .::::::::::::::::. ")
+  fmt.Println(" O::::::::::::::::O ")
+  fmt.Println(" -::::::::::::::::- ")
+  fmt.Println(" `::::::::::::::::` ")
+  fmt.Println("  .::::::::::::::.  ")
+  fmt.Println("    oO:::::::Oo     ")
+}
